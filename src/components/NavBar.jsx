c80125998370fea19002166c1ad1e0cd081de4a9
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-08-11 22:44:04 +0530

    Complete Responsive Nav Bar Section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/logo/Logo1.png";
 import "./navbar.css";
 import { links } from "../data";
 import { FaBars } from "react-icons/fa";
+import { MdOutlineClose } from "react-icons/md";
 
 const NavBar = () => {
+  const [isNavShowing, setIsNavShowing] = useState(false);
   return (
     <nav>
       <div className="container nav__container">
-        <Link to="/" className="logo">
+        <Link to="/" className="logo" onClick={() => setIsNavShowing(false)}>
           <img src={Logo} alt="navlogo" />
         </Link>
-        <ul className="nav__links">
+        <ul
+          className={`nav__links ${isNavShowing ? "show__nav" : "hide__nav"}`}
+        >
           {links.map(({ name, path }, index) => {
             return (
-              <li>
-                <NavLink to={path}>{name}</NavLink>
+              <li key={index}>
+                <NavLink
+                  to={path}
+                  className={({ isActive }) => (isActive ? "active-nav" : "")}
+                  onClick={() => setIsNavShowing((prev) => !prev)}
+                >
+                  {name}
+                </NavLink>
               </li>
             );
           })}
         </ul>
-        <button className="nav__toggle-btn">
-          <FaBars />
+        <button
+          className="nav__toggle-btn"
+          onClick={() => setIsNavShowing((prev) => !prev)}
+        >
+          {isNavShowing ? <MdOutlineClose /> : <FaBars />}
         </button>
       </div>
     </nav>
