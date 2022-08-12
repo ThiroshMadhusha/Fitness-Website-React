98f2b6416dfcf53a630b3fa888a48db093ad509b
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-08-12 23:58:26 +0530

    Complete Home section and css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import Header from '../../components/Header'
-import "./home.css"
+import React from "react";
+import FAQs from "../../components/FAQs";
+import Footer from "../../components/Footer";
+import MainHeader from "../../components/MainHeader";
+import Program from "../../components/Program";
+import Testimonials from "../../components/Testimonials";
+import Values from "../../components/Values";
+import "./home.css";
 
 const Home = () => {
   return (
-    <Header/>
-  )
-}
+    <>
+      <MainHeader />
+      <Program />
+      <Values />
+      <FAQs />
+      <Testimonials />
+      <Footer />
+    </>
+  );
+};
 
-export default Home
+export default Home;
