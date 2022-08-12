630a6c6b04453cd173d45c6856fb98e940e4de1a
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-08-12 23:57:05 +0530

    Complete component Header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const Header = () => {
+const Header = ({ title, image, children }) => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <div className="header__container">
+        <div className="header__container-bg">
+          <img src={image} alt="headerBG" />
+        </div>
+        <div className="header__content">
+          <h2>{title}</h2>
+          <p>{children}</p>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
